f9a44b35616fe8b3f32e57270b56392331cf1a64
Johnny <JohnnyLieu@yahoo.com>
2022-08-14 15:40:39 -0700

    bkacground of button turns grey on click, after 100 ms it will disappear

--- game.js
+++ game.js
@@ -2,17 +2,25 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
-var nextSequence = () => {
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColor = buttonColors[randomNumber];
-    playSound(randomChosenColor);
-    gamePattern.push(randomChosenColor);
+var animatePress = (currentColor) => {
+    $(`#${currentColor}`).addClass('pressed');
+    setTimeout(function() {
+        $(`#${currentColor}`).removeClass('pressed')
+    }, 100);
 }
 
 var playSound = (name) => {
     var audio = new Audio(`./sounds/${name}.mp3`)
     audio.play();
     $(`#${name}`).fadeOut(100).fadeIn(100);
+    animatePress(name);
+}
+
+var nextSequence = () => {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    playSound(randomChosenColor);
+    gamePattern.push(randomChosenColor);
 }
 
 $(".btn").on('click', function(e){
