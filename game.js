0b34959eea2dc96ea520298fb3ce53e31d03f7ca
Johnny <JohnnyLieu@yahoo.com>
2022-08-14 13:32:07 -0700

    added flashing and sound play to its own function

--- game.js
+++ game.js
@@ -1,14 +1,22 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
 var nextSequence = () => {
-    randomNumber = Math.floor(Math.random() * 4);
+    var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
+    playSound(randomChosenColor);
     gamePattern.push(randomChosenColor);
 }
 
-$(".btn").on('click', function(e){
-    var audio = new Audio(`./sounds/${e.target.id}.mp3`)
+var playSound = (name) => {
+    var audio = new Audio(`./sounds/${name}.mp3`)
     audio.play();
-    $(`#${e.target.id}`).fadeOut(100).fadeIn(100);
+    $(`#${name}`).fadeOut(100).fadeIn(100);
+}
+
+$(".btn").on('click', function(e){
+    var userChosenColor = e.target.id;
+    playSound(userChosenColor);
+    userClickedPattern.push(userChosenColor);
 })
