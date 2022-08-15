af4a2582ed80d2bee4463cc83914e266f95da88e
Johnny <JohnnyLieu@yahoo.com>
2022-08-14 19:26:57 -0700

    app able to check user input along with game input and initializes nextSequence() to continue game if user inputs are correct

--- game.js
+++ game.js
@@ -5,37 +5,46 @@ var level = 0;
 var gameStarted = false;
 
 $(document).keydown(function() {
-    if (gameStarted === false) {
+    if (gameStarted == false) {
         nextSequence();
+        console.log('1');
         gameStarted = true;
-        console.log(gamePattern);
     }
 })
 
 var animatePress = (currentColor) => {
-    $(`#${currentColor}`).addClass('pressed');
-    setTimeout(function() {
-        $(`#${currentColor}`).removeClass('pressed')
-    }, 100);
+    $(`#${currentColor}`).addClass('pressed').delay(100).removeClass('pressed');
 }
 
 var playSound = (name) => {
     var audio = new Audio(`./sounds/${name}.mp3`)
     audio.play();
     $(`#${name}`).fadeOut(100).fadeIn(100);
-    animatePress(name);
 }
 
 var nextSequence = () => {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
-    playSound(randomChosenColor);
     gamePattern.push(randomChosenColor);
+    playSound(randomChosenColor);
+    animatePress(randomChosenColor);
     level++;
+    $(`#level-title`).text(`Level ${level}`);
+}
+
+var checkAnswer = (currentLevel) => {
+    console.log(`user: ${userClickedPattern}\ngame: ${gamePattern}`);
+    if (userClickedPattern.join() == gamePattern.join()) {
+        setTimeout(function() {
+            nextSequence();
+            userClickedPattern = [];
+        }, 500);
+    }
 }
 
 $(".btn").on('click', function(e){
     var userChosenColor = e.target.id;
     playSound(userChosenColor);
     userClickedPattern.push(userChosenColor);
+    checkAnswer(userChosenColor);
 })
