c70df93bc668ec8cedc0f685f7560ea4edf32cdc
Johnny <JohnnyLieu@yahoo.com>
2022-11-13 14:06:48 -0800

    modified index.html to ejs succesful, able to connect to db, but unable to locate sound folder

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+_ = require('lodash');
+const {test123} = require('./noneyo.js');
+
+port = process.env.PORT || 3000;
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect(`mongodb+srv://jlieu83:${test123}@cluster0.lkecag0.mongodb.net/simongameDB`)
+
+const scoreSchema = new mongoose.Schema({
+    name: String,
+    store: Number
+});
+
+const Score = mongoose.model('Score', scoreSchema);
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log(`Server started on port ${port}`);
+  });
